90cda737dcb1100728087662217c541dd53c0f15
Zanardelli Julien <jzanardelli@editpress.lu>
2019-04-04 13:03:58 +0200

    first commit

---
+++ editpress-paywall.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: Editpress Paywall
+*/
+
+/*
+ * Plugin constants
+ */
+
+if(!defined('EDITPAYWALL_URL'))
+	define('EDITPAYWALL__URL', plugin_dir_url( __FILE__ ));
+if(!defined('EDITPAYWALL__PATH'))
+	define('EDITPAYWALL__PATH', plugin_dir_path( __FILE__ ));
+ 
+
+
+    /**
+ * Class Feedier
+ *
+ * This class creates the option page and add the web app script
+ */
+class EditpressPaywall
+{
+ 
+    /**
+     * Feedier constructor.
+     *
+     * The main plugin actions registered for WordPress
+     */
+    public function __construct()
+    {
+ 
+    }
+ 
+}
+ 
+/*
+ * Starts our plugin class, easy!
+ */
+new EditpressPaywall();
+
+?>
