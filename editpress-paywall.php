478817476037f3b16cb7465128be05f284553b14
Zanardelli Julien <jzanardelli@editpress.lu>
2019-04-04 16:33:57 +0200

    Config DotEnv and Auth0

--- editpress-paywall.php
+++ editpress-paywall.php
@@ -5,18 +5,31 @@ Plugin Name: Editpress Paywall
 
 
 require __DIR__ . '/vendor/autoload.php';
-use Auth0\SDK\Auth0;
 
+use Auth0\SDK\Auth0;
+use josegonzalez\Dotenv\Loader;
+
+/*
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+*/
 
 /*
  * Plugin constants
  */
-
 if(!defined('EDITPAYWALL_URL'))
 	define('EDITPAYWALL_URL', plugin_dir_url( __FILE__ ));
 if(!defined('EDITPAYWALL_PATH'))
 	define('EDITPAYWALL_PATH', plugin_dir_path( __FILE__ ));
  
+/*
+* Loader Env File
+*/
+$Dotenv = new Loader(__DIR__ . '/.env');
+$Dotenv->parse()->putenv(true);
+
+// Get environment variables
+echo 'My Auth0 domain is ' . getenv('AUTH0_DOMAIN');
 
 
     /**
@@ -44,6 +57,6 @@ class EditpressPaywall
  */
 new EditpressPaywall();
 
-echo (EDITPAYWALL_URL);
+
 
 ?>
