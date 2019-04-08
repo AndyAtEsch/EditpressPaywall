c36f586b20e75aad2973562723a807760bec4e76
Zanardelli Julien <jzanardelli@editpress.lu>
2019-04-08 15:53:08 +0200

    Update Shortcut

--- templates/profile.php
+++ templates/profile.php
@@ -1,26 +1,33 @@
 <?php
 
 
-echo "<h1>Profile Page 12</h1>";
+echo "<h1>Profile Page 17</h1>";
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 require  __DIR__.'/../vendor/autoload.php';
 
+//ChargeBee_Environment::configure(getenv('CHARGEBEE_SITE'),getenv('CHARGEBEE_SITE_API_KEY'));
+
 use Auth0\SDK\Auth0;
+use Auth0\SDK\Store\SessionStore;
+use Auth0\SDK\API\Management;
+use Auth0\SDK\API\Authentication;
 
-/*
-$auth0 = new Auth0([
-    'domain' => getenv('AUTH0_DOMAIN'),
-    'client_id' => getenv('AUTH0_CLIENT_ID'),
-    'client_secret' => getenv('AUTH0_CLIENT_SECRET'),
-    'redirect_uri' => getenv('AUTH0_REDIRECT_URI'),
-    'scope' => 'openid',
-  ]);
-*/
-  $userInfo = $auth0->getUser();
 
-  ChargeBee_Environment::configure(getenv('CHARGEBEE_SITE'),getenv('CHARGEBEE_SITE_API_KEY'));
+var_dump(EditpressPaywall::auth0UserIsConnected());
+
+$user = EditpressPaywall::auth0UserIsConnected();
+
+if( $user ){
+
+    $management_api = EditpressPaywall::managementApiAuth();
+
+    $costumer_info = $management_api->users->get($user['sub']);
+
+    var_dump($costumer_info);
+
+}
 
 
 ?>
